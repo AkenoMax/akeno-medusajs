a1d1e832da903c712feba1fa85b2472938e7c031
AkenoMax <122606686+AkenoMax@users.noreply.github.com>
2023-01-14 13:11:41 +0100

    stripe added

--- medusa-config.js
+++ medusa-config.js
@@ -35,23 +35,18 @@ const DATABASE_URL =
 // Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = process.env.REDIS_URL || "redis://default:xtwTl029gEfo37ovJcbU@containers-us-west-181.railway.app:6808";
 
-// Stripe keys
-const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
-const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
-
 // This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
-  // Uncomment to add Stripe support.
-  // You can create a Stripe account via: https://stripe.com
-  // {
-  //   resolve: `medusa-payment-stripe`,
-  //   options: {
-  //     api_key: STRIPE_API_KEY,
-  //     webhook_secret: STRIPE_WEBHOOK_SECRET,
-  //   },
-  // },
+
+     {
+       resolve: `medusa-payment-stripe`,
+       options: {
+         api_key: STRIPE_API_KEY,
+         webhook_secret: STRIPE_WEBHOOK_SECRET,
+       },
+     },
 ];
 
 module.exports = {
