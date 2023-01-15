052f94b20cecfe8b7539c87650ea42e9b6f05ff8
AkenoMax <122606686+AkenoMax@users.noreply.github.com>
2023-01-15 19:35:33 +0100

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -23,10 +23,10 @@ try {
 }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "*,*.akeno.pl";
+const ADMIN_CORS = process.env.ADMIN_CORS || ".*,*.akeno.pl";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "*,*.akeno.pl";
+const STORE_CORS = process.env.STORE_CORS || ".*,*.akeno.pl";
 
 // This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
